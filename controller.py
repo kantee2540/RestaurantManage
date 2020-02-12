0577e6d54ea59b6b955868258e20997d6bfe2657
Omp <kantee2540@hotmail.com>
2020-02-12 21:56:08 +0700

    login feature

--- controller.py
+++ controller.py
@@ -7,8 +7,21 @@ import pymongo
 import main
 import register
 import login
+
 import random
 import hashlib
+
+
+def get_database_client():
+    return pymongo.MongoClient("mongodb+srv://kantee2540:K%61n%742540@cluster0-ww6d1.mongodb.net/test?retryWrites=true&w=majority")
+
+
+def message_box(title, text_message):
+    message = QtWidgets.QMessageBox()
+    message.setIcon(QtWidgets.QMessageBox.Information)
+    message.setText(text_message)
+    message.setWindowTitle(title)
+    message.exec_()
 
 
 class LoginPage(QtWidgets.QMainWindow):
@@ -22,8 +35,30 @@ class LoginPage(QtWidgets.QMainWindow):
         self.register_window = RegisterPage()
 
     def login_click(self):
-        self.main_window.show()
-        self.hide()
+
+        #self.main_window.show()
+        self.login_authen()
+
+    def login_authen(self):
+        try:
+            client = get_database_client()
+            db = client.get_database("Restaurant")
+
+            username = self.ui.user_edit.text()
+            password = hashlib.md5(self.ui.pass_edit.text().encode()).hexdigest()
+            login_user = db.User.find_one({"username": username})
+            if login_user:
+                if password == login_user["password"]:
+                    self.hide()
+                    self.main_window.show()
+                else:
+                    message_box("Login", "Username or password incorrect please try again")
+
+            else:
+                message_box("Login", "Username or password incorrect please try again")
+
+        except Exception as e:
+            print("Error = {}".format(e))
 
     def register_click(self):
         self.register_window.show()
@@ -104,7 +139,7 @@ class RegisterPage(QtWidgets.QMainWindow):
 
     def register_process(self):
         try:
-            client = pymongo.MongoClient("mongodb+srv://kantee2540:K%61n%742540@cluster0-ww6d1.mongodb.net/test?retryWrites=true&w=majority")
+            client = get_database_client()
             db = client.get_database("Restaurant")
 
             restaurant_name = self.ui.rest_edit.text()
@@ -115,21 +150,12 @@ class RegisterPage(QtWidgets.QMainWindow):
             data = {"restaurant_name": restaurant_name, "username": username, "password": password_encrypt}
             rs = db.User.insert_one(data)
             if rs:
-                message = QtWidgets.QMessageBox()
-                message.setIcon(QtWidgets.QMessageBox.Information)
-                message.setText("Register successful!\n Your restaurant name : {}".format(username))
-                message.setWindowTitle("Register")
-                message.exec_()
+                message_box("Register", "Register successful!\n Your restaurant name : {}".format(username))
                 self.clear_lineedit()
                 self.close()
 
         except Exception as e:
-            print("Error = {}".format(e))
-            error_dialog = QtWidgets.QMessageBox()
-            error_dialog.setIcon(QtWidgets.QMessageBox.Warning)
-            error_dialog.setText("Error message : {}\n Please Contact ch.kantee_st@tni.ac.th".format(e))
-            error_dialog.setWindowTitle("Error")
-            error_dialog.exec_()
+            message_box("Error", "Error message : \"{}\"Please Contact ch.kantee_st@tni.ac.th".format(e))
 
     def clear_lineedit(self):
         self.ui.rest_edit.clear()
